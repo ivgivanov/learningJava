9cbec358890db947def2388bda6d067e0df35225
Ivaylo Ivanov <ivaylo@ivgivanov.info>
2020-07-18 23:21:02 +0200

    controllers

---
+++ conference-spring_boot/src/main/java/info/ivgivanov/conference/model/Registration.java
@@ -0,0 +1,17 @@
+package info.ivgivanov.conference.model;
+
+public class Registration {
+	
+	private String name;
+
+	public String getName() {
+		return name;
+	}
+
+	public void setName(String name) {
+		this.name = name;
+	}
+	
+	
+
+}
